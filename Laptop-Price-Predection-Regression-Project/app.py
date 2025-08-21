62e12b7715523654e6bb7ac11c414f415f24f1eb
Rituraj Singh <87235235+rjdecore@users.noreply.github.com>
2025-08-21 16:57:02 +0530

    Add files via upload

---
+++ Laptop-Price-Predection-Regression-Project/app.py
@@ -0,0 +1,91 @@
+import streamlit as st
+import joblib
+import numpy as np
+import pandas as pd
+
+# import the model and dataframe
+pipe = joblib.load(open('pipe.pkl','rb'))
+df = joblib.load(open('df.pkl','rb'))
+
+st.title("💻 Laptop Price Predictor")
+
+# brand
+company = st.selectbox('Brand', df['Company'].unique())
+
+# type of laptop
+laptop_type = st.selectbox('Type', df['TypeName'].unique())
+
+# Ram
+ram = st.selectbox('RAM (in GB)', [2, 4, 6, 8, 12, 16, 24, 32, 64])
+
+# weight
+weight = st.number_input('Weight of the Laptop')
+
+# Touchscreen
+touchscreen = st.selectbox('Touchscreen', ['No', 'Yes'])
+
+# IPS
+ips = st.selectbox('IPS', ['No', 'Yes'])
+
+# screen size
+screen_size = st.slider('Screen size (in inches)', 10.0, 18.0, 13.0)
+
+# resolution
+resolution = st.selectbox(
+    'Screen Resolution',
+    ['1920x1080', '1366x768', '1600x900', '3840x2160',
+     '3200x1800', '2880x1800', '2560x1600', '2560x1440', '2304x1440']
+)
+
+# CPU
+cpu = st.selectbox('CPU', df['Cpu brand'].unique())
+
+# HDD
+hdd = st.selectbox('HDD (in GB)', [0, 128, 256, 512, 1024, 2048])
+
+# SSD
+ssd = st.selectbox('SSD (in GB)', [0, 8, 128, 256, 512, 1024])
+
+# GPU
+gpu = st.selectbox('GPU', df['Gpu brand'].unique())
+
+# OS
+os = st.selectbox('OS', df['os'].unique())
+
+if st.button('Predict Price'):
+    # Preprocessing inputs
+    if touchscreen == 'Yes':
+        touchscreen = 1
+    else:
+        touchscreen = 0
+
+    if ips == 'Yes':
+        ips = 1
+    else:
+        ips = 0
+
+    X_res = int(resolution.split('x')[0])
+    Y_res = int(resolution.split('x')[1])
+    ppi = ((X_res**2) + (Y_res**2))**0.5 / screen_size
+
+    # Create DataFrame with correct column names
+       # Create DataFrame with correct column names (match training dataset exactly)
+    query_df = pd.DataFrame({
+        'Company': [company],
+        'TypeName': [laptop_type],
+        'Ram': [ram],
+        'Weight': [weight],
+        'Touchscreen': [touchscreen],
+        'Ips': [ips],
+        'ppi': [ppi],
+        'Cpu brand': [cpu],
+        'HDD': [hdd],     # 🔥 uppercase
+        'SSD': [ssd],     # 🔥 uppercase
+        'Gpu brand': [gpu],
+        'os': [os]
+    })
+
+
+    # Prediction
+    predicted_price = int(np.exp(pipe.predict(query_df)[0]))
+    st.subheader(f"💰 The predicted price of this configuration is: **₹ {predicted_price}**")
